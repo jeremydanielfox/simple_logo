f6d5f1414fb9612ddc03d1d7e06ae887a900194b
plm17 <plm17@duke.edu>
2015-02-20 19:33:54 -0500

    Changed TurtleView to Canvas. Still have a weird error in Display

--- src/display/Display.java
+++ src/display/Display.java
@@ -1,11 +1,17 @@
 package display;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import turtle_view.TurtleView;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 
 public class Display {
@@ -19,14 +25,55 @@ public class Display {
 		myRoot = new BorderPane();
 		
 		myRoot.setTop(makeMenuBar());
+		myRoot.setCenter(makeTurtleView());
 		
 		myScene = new Scene(myRoot, Integer.parseInt(myValues.getString("Width")), Integer.parseInt(myValues.getString("Height")));
 	}
 	
+	private Node makeTurtleView() {
+		Canvas tv = new Canvas(Integer.parseInt(myValues.getString("TV_Width")), Integer.parseInt(myValues.getString("TV_Height")));
+		return tv;
+	}
+
 	private Node makeMenuBar() {
-		Menu fileMenu = new Menu(myValues.getString("File"));
-		Display.class.getDeclaredMethod(name, parameterTypes)
-		return null;
+		
+		MenuBar menuBar = new MenuBar();
+		try {
+		menuBar.getMenus().add(makeMenu("File"));
+		menuBar.getMenus().add(makeMenu("Edit"));
+		menuBar.getMenus().add(makeMenu("View"));
+		menuBar.getMenus().add(makeMenu("Options"));
+		menuBar.getMenus().add(makeMenu("Help"));
+		} catch (NoSuchMethodException e){
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return menuBar;
+	}
+
+	private Menu makeMenu(String name) throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Menu menu = new Menu(name);
+		String[] arrayCharles = myValues.getString(name).split(", ");
+		for (String s : arrayCharles) {
+			MenuItem item = new MenuItem();
+			Method m = Display.class.getDeclaredMethod(getMethodName(s));
+			//not sure why I'm getting this error
+			item.setOnAction(e -> m.invoke(null, null));
+			menu.getItems().add(item);
+		}
+		return menu;
+	}
+	
+	private String getMethodName(String s) {
+		s.replaceAll(" ", "");
+		String first = String.valueOf(s.charAt(0)); 
+		s.replaceFirst("%c", first.toLowerCase());
+		return s;
 	}
 
 	public Scene getScene() {
