291545facd1acfe453dffe8dc0dc51e14c7acd94
Nathan Prabhu <nprab428@yahoo.com>
2015-02-23 18:07:41 -0500

    add getter for varsMap

--- src/model/node/TreeNodeInfo.java
+++ src/model/node/TreeNodeInfo.java
@@ -31,10 +31,6 @@ public class TreeNodeInfo {
         return myNode;
     }
 
-    public int getChildNumber () {
-        return myChildNames.size();
-    }
-
     public String getNextChildName () {
         return myChildNames.remove();
     }
