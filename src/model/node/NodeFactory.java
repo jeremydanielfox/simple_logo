0c209765c88bb10757824dfbdfd2b94ccd58e233
Nathan Prabhu <nprab428@yahoo.com>
2015-02-26 01:26:20 -0500

    fixed initial turtle update bug, turtle should now rotate

--- src/model/node/NodeFactory.java
+++ src/model/node/NodeFactory.java
@@ -3,6 +3,7 @@ package model.node;
 import model.Turtle;
 import model.node.iteration.Repeat;
 import model.node.mathOperation.TwoArgMathOperation;
+import model.node.turtleCommand.Rotation;
 import model.node.turtleCommand.Translation;
 
 public final class NodeFactory {
@@ -27,8 +28,10 @@ public final class NodeFactory {
                 return new Translation(true, turtle);
             case "Backward":
                 return new Translation(false, turtle);
+            case "Right":
+                return new Rotation(true, turtle);
             case "Left":
-                //return new Rotation(false, turtle);
+                return new Rotation(false, turtle);
             case "Sum":
                 return new TwoArgMathOperation("+");
             case "Repeat":
