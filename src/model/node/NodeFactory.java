add4dcd08ad9bd0587ef376a97899e0fe614f01a
Nathan Prabhu <nprab428@yahoo.com>
2015-02-26 15:38:12 -0500

    add reflection capabilities in Node Factory

--- src/model/node/NodeFactory.java
+++ src/model/node/NodeFactory.java
@@ -1,46 +1,53 @@
 package model.node;
 
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import model.Turtle;
 import model.node.iteration.Repeat;
 import model.node.mathOperation.TwoArgMathOperation;
+import model.node.turtleCommand.Forward;
 import model.node.turtleCommand.Rotation;
 import model.node.turtleCommand.Translation;
+
 
 public final class NodeFactory {
 
     private static NodeFactory instance;
+    private List<String> turtleCommands = new ArrayList<String>(Arrays.asList(new String[]{
+                                                                                           "Forward",
+                                                                                           "Backward",
+                                                                                           "Left",
+                                                                                           "Right"
+    }));
 
     private NodeFactory () {
     }
 
-    // singleton may be dangerous... 
+    // singleton may be dangerous...
     public static synchronized NodeFactory getInstance () {
         if (instance == null)
             instance = new NodeFactory();
         return instance;
     }
-    
-    public enum TurtleEnum {
-        FORWARD, BACKWARD, LEFT, RIGHT;
-    }
+
 
     // unfortunate that we have to use case/switch for every command. But alternative would be to
     // create unnecessary amount of classes, if reflection or oodesign factory pattern were used
     public TreeNode getNonConstant (String key, Turtle turtle) {
-        if (Arrays.asList(TurtleEnum.values()).contains(TurtleEnum.valueOf(key))){
-            // do reflection
+        if (isTurtleCommand(key)) { 
+            try {
+                return reflectionFactory(key, turtle);
+            }
+            catch (Exception e) {
+                // not sure how to handle this one
+                e.printStackTrace();
+                throw new RuntimeException();
+            }
         }
-        
+
         switch (key) {
-            case "Forward":
-                return new Translation(true, turtle);
-            case "Backward":
-                return new Translation(false, turtle);
-            case "Right":
-                return new Rotation(true, turtle);
-            case "Left":
-                return new Rotation(false, turtle);
             case "Sum":
                 return new TwoArgMathOperation("+");
             case "Repeat":
@@ -48,6 +55,30 @@ public final class NodeFactory {
             default:
                 // throw "key not found" exception... shouldn't happen though
                 return null;
+        }
+    }
+
+    private boolean isTurtleCommand (String key) {
+        return turtleCommands.contains(key);
+    }
+
+    private TreeNode reflectionFactory (String key, Turtle turtle) throws Exception {
+        try {
+            Class<?> targetClass = Class.forName(String.format("model.node.turtleCommand.%s", key));
+            try {
+                Constructor<?> constructor = targetClass.getConstructor(Turtle.class);
+                return (TreeNode) constructor.newInstance(turtle);
+            }
+            catch (NoSuchMethodException | SecurityException e) {
+                System.err.println("incorrect constructor");
+                e.printStackTrace();
+                throw new RuntimeException(); // do something other than throw error to stop program
+            }
+        }
+        catch (ClassNotFoundException e) {
+            System.err.println("TurtleEnum error");
+            e.printStackTrace();
+            throw new RuntimeException(); // do something other than throw error to stop program
         }
     }
 
